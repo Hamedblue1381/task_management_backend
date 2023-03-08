7b0db83f2fced9c3bf12d5aea60c7b20fb0e163d
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-03-08 18:32:38 +0330

    first commit

---
+++ src/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Tasks struct {
+	ID         string `json:"id"`
+	TaskName   string `json:"task_name"`
+	TaskDetail string `json:"task_detail"`
+	Date       string `json:"date"`
+}
+
+var tasks []Tasks
+
+func allTasks() {
+	task := Tasks{
+		ID:         "1",
+		TaskName:   "New project",
+		TaskDetail: "i'm backend bro",
+		Date:       "2023-05-03",
+	}
+	tasks = append(tasks, task)
+	fmt.Println("your tasks are :\n", tasks)
+	task1 := Tasks{
+		ID:         "2",
+		TaskName:   "Reza",
+		TaskDetail: "i'm reza sister",
+		Date:       "2023-05-03",
+	}
+	tasks = append(tasks, task1)
+	fmt.Println("your tasks are :", tasks)
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("i'm homepage")
+}
+func gettask(w http.ResponseWriter, r *http.Request) {
+	taskId := mux.Vars(r)
+	flag := false
+	for i := 0; i < len(tasks); i++ {
+		if taskId["id"] == tasks[i].ID {
+			json.NewEncoder(w).Encode(tasks[i])
+			flag = true
+			break
+		}
+	}
+	if flag == false {
+		json.NewEncoder(w).Encode(map[string]string{"status": "Error"})
+	}
+}
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var task Tasks
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	task.ID = strconv.Itoa(rand.Intn(1000))
+	currentTime := time.Now().Format("01-02-2006")
+	task.Date = currentTime
+	tasks = append(tasks, task)
+	json.NewEncoder(w).Encode(task)
+}
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("i'm homepage")
+}
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("i'm homepage")
+}
+
+func handleRoutes() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/gettask/{id}", gettask).Methods("GET")
+	router.HandleFunc("/gettasks", getTasks).Methods("GET")
+	router.HandleFunc("/create", createTask).Methods("POST")
+	router.HandleFunc("/delete/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/update/{id}", updateTask).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8082", router))
+}
+
+func main() {
+	allTasks()
+	handleRoutes()
+}
